2b651c1e11a239981e62c2f3445c78f62d751b0c
Eric Morgan <accounts@ericcolvinmorgan.com>
2018-11-11 15:30:22 +0100

    Added additional comments

--- Week7Assignment/checker.js
+++ Week7Assignment/checker.js
@@ -1,55 +1,71 @@
+// Path detail used to easily configure dev and prod environments
+// Code adapted from documentation at https://nodejs.org/api/path.html
 var path = require('path');
 var viewsPath = path.join(__dirname, '/views');
 var layoutsPath = path.join(viewsPath, '/layouts');
 
+//Adapted from Lecture and Express documentation at https://expressjs.com/en/guide/using-template-engines.html
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({layoutsDir:layoutsPath, defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('views', viewsPath);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 6543);
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//Below is all adapted from the lecture.
+//Home Page
 app.get('/',function(req,res){
-  res.render('home') //We can omit the .handlebars extension as we do below
+  res.render('home') 
 });
 
+//Data - GET
 app.get('/data',function(req,res){
-  var qParams = [];
+  
+  //Build Detail From Query
+  var queryDetail = [];
   for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
+    queryDetail.push({'name':p,'value':req.query[p]})
   }
+
   var context = {};
   context.type = req.method;
-  context.dataList = qParams;
-  res.render('data', context) //We can omit the .handlebars extension as we do below
+  context.queryDetail = queryDetail;
+  res.render('data', context) 
 });
 
+//Data - POST
 app.post('/data',function(req,res){
-  var qParams = [];
+  //Build Detail From Query
+  var queryDetail = [];
   for (var p in req.query){
-    qParams.push({'name':p,'value':req.query[p]})
+    queryDetail.push({'name':p,'value':req.query[p]})
   }
+
+  //Build Detail From Body
+  var bodyDetail = [];  
   for (var p in req.body){
-    qParams.push({'name':p,'value':req.body[p]})
+    bodyDetail.push({'name':p,'value':req.body[p]})
   }
-  console.log(qParams);
-  console.log(req.body);
+
   var context = {};
   context.type = req.method;
-  context.dataList = qParams;
-  res.render('data', context) //We can omit the .handlebars extension as we do below
+  context.queryDetail = queryDetail;
+  context.bodyDetail = bodyDetail;
+  res.render('data', context)
 });
 
+//Handle 404
 app.use(function(req,res){
   res.status(404);
   res.send('404 - Not Found');
 });
 
+//Handle 500
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.type('plain/text');
@@ -57,6 +73,7 @@ app.use(function(err, req, res, next){
   res.send('500 - Server Error');
 });
 
+//Start Server
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
